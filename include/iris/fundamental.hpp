944ef4adf909c88c45b551d9ab5eee023ec229a3
acd1034 <48613285+acd1034@users.noreply.github.com>
2023-04-29 15:21:18 +0900

    Add NS_RESULT_AUTO

--- include/iris/fundamental.hpp
+++ include/iris/fundamental.hpp
@@ -12,10 +12,20 @@
 #include <tuple>
 #include <type_traits>
 #include <utility>
+#include <variant>
+
+#define NS_RESULT_AUTO_IMPL(v, r)                                              \
+  auto&& ns_tmp_##v = (r);                                                     \
+  if (ns_tmp_##v.index() == 1)                                                 \
+    return std::get<1>(std::forward<decltype(ns_tmp_##v)>(ns_tmp_##v));        \
+  [[maybe_unused]] auto&& v =                                                  \
+    std::get<0>(std::forward<decltype(ns_tmp_##v)>(ns_tmp_##v))
+
+#define NS_RESULT_AUTO(v, r) NS_RESULT_AUTO_IMPL(v, r)
 
 namespace ns {
   struct Error {
     std::string_view msg;
     friend bool operator==(const Error&, const Error&) = default;
   };
-}
+} // namespace ns
